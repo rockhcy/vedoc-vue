23238f1e3f6add5a6ccbcdc1e2c716a88f854879
hcy <337815986@qq.com>
2020-08-31 00:05:31 +0800

    解决跨域的问题

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,12 +1,12 @@
-// import axios from 'axios';
+import axios from 'axios'
 
-// window.isRefreshing = false  //token正在刷新中
-// const Axios = axios.create({
-//   baseURL: 'http://192.168.50.143:9999/',//请求地址
-//   withCredentials: true,//允许跨域
-//   headers: {
-//     'Content-Type': "application/json;charset=UTF-8",
-//   }
-// });
+// 创建axios实例
+const Axios = axios.create({
+  baseURL: 'http://localhost:10002/', // api的base_url
+  timeout: 5000, // 请求超时时间
+  withCredentials: false,// 当前请求为跨域类型时是否在请求中协带cookie,需要注意是，当配置了xhr.withCredentials = true时，必须在后端增加 response 头信息Access-Control-Allow-Origin，且必须指定域名，而不能指定为*。
+  crossDomain: true,//允许跨域
+  headers: { 'Content-Type': "application/json;charset=UTF-8" }
+})
 
-// export default Axios;
+export default Axios
--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import router from '@/config/router.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/reset.css'
 import 'element-ui/lib/theme-chalk/index.css'
-import Axios from 'axios';
+import axios from './config/axiosConfig.js';
 import store from '@/config/store'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-Vue.prototype.$api = Axios;
-Axios.defaults.baseURL = 'http://192.168.50.143:9999/'
-Axios.defaults.withCredentials = true
-Axios.defaults.headers = { 'Content-Type': "application/json;charset=UTF-8" }
-
+Vue.prototype.$api = axios;
 
 
 new Vue({
